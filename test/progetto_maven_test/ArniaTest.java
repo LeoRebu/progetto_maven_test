3abdc061f5793f17b1c12d96655b55ab0d332e4c
LeoRebu <71372855+LeoRebu@users.noreply.github.com>
2020-09-29 09:55:55 +0200

    cambiate 2 parole per vedere se arriva a gitlab

--- test/progetto_maven_test/ArniaTest.java
+++ test/progetto_maven_test/ArniaTest.java
@@ -18,7 +18,7 @@ public class ArniaTest {
 		//a1 = new Arnia("aa",true,false,0); //caso corretto 2
 		
 		a1 = new Arnia("aa",true,true,0); //caso errato
-		//a1 = new Arnia("aa",true,true,-1); //caso ???
+		//a1 = new Arnia("aa",true,true,-1); //caso ancora errato
 		//a1 = new Arnia("aa",false,true,5); //caso disastroso
 	}
 	
