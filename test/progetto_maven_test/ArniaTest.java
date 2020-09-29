5e0b4b69693d794662b59ae58ed67c368679ab11
LeoRebu <71372855+LeoRebu@users.noreply.github.com>
2020-09-29 11:07:39 +0200

    test finale per verificare commit su gitlab

--- test/progetto_maven_test/ArniaTest.java
+++ test/progetto_maven_test/ArniaTest.java
@@ -19,7 +19,7 @@ public class ArniaTest {
 		
 		a1 = new Arnia("aa",true,true,0); //caso errato
 		//a1 = new Arnia("aa",true,true,-1); //caso errato 2
-		//a1 = new Arnia("aa",false,true,5); //caso disastroso
+		//a1 = new Arnia("aa",false,true,5); //caso disastroso!
 	}
 	
 	
